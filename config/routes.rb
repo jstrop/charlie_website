53838523d761569706be23c415b9b450edb192bc
Jared Strop <jstrop21@gmail.com>
2011-10-10 00:45:58 -0400

    Done with sample pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 CharlieWebsite::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/contact"
+
+  get "pages/about"
+
+  get "pages/music"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
